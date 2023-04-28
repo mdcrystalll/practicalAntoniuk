49a9a63e543253255960e2473f26b121e4e5ef01
mdcrystalll <dimaant20@gmail.com>
2023-04-28 09:34:10 +0300

    pz 1.9 done

---
+++ pz_1.9/task_1.js
@@ -0,0 +1,14 @@
+let num = 2;
+while (num <= 100) {
+  let isPrime = true;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
+    console.log(num);
+  }
+  num++;
+}
